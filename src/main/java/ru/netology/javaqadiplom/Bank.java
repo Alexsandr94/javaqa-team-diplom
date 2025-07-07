1828a5c53a718ecff9e206a6d22986d7b941d871
Alexsandr94 <lisanov.94@mail.ru>
2025-07-07 21:41:58 +0500

    Bug fixes in issues #23, 24, 25, 26, 27, 28, 29

--- src/main/java/ru/netology/javaqadiplom/Bank.java
+++ src/main/java/ru/netology/javaqadiplom/Bank.java
@@ -19,8 +19,12 @@ public class Bank {
             return false;
         }
         if (from.pay(amount)) {
-            to.add(amount);
-            return true;
+            if (to.add(amount)) {
+                return true;
+            } else {
+                from.add(amount);
+                return false;
+            }
         }
         return false;
     }
